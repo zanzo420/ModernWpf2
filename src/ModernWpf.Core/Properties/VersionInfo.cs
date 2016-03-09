68b08970379efda51801a615f762f39e2ff12083
Eugene Wang <yinchun.wang@gmail.com>
2016-03-08 20:53:55 -0500

    Initial structure.

---
+++ src/ModernWpf.Core/Properties/VersionInfo.cs
@@ -0,0 +1,20 @@
+﻿using ModernWpf;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Yin-Chun Wang")]
+[assembly: AssemblyCopyright("Copyright © Yin-Chun Wang 2016")]
+
+[assembly: AssemblyVersion(VersionInfo.MajorVersion)]
+[assembly: AssemblyFileVersion(VersionInfo.BuildVersion)]
+[assembly: AssemblyInformationalVersion(VersionInfo.BuildVersion)]
+
+namespace ModernWpf
+{
+    static class VersionInfo
+    {
+        // keep this same in major releases
+        public const string MajorVersion = "2.0.0.0";
+        // change this for each nuget release
+        public const string BuildVersion = "2.0.0";
+    }
+}
