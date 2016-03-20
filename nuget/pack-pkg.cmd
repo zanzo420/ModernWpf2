26991416aa308d2d3ef0a89706e33c81cc08b036
Eugene Wang <yinchun.wang@gmail.com>
2016-03-20 10:02:25 -0400

    update package info.

---
+++ nuget/pack-pkg.cmd
@@ -0,0 +1,3 @@
+nuget pack ..\src\ModernWpf.Core\ModernWpf.Core.csproj -Prop Configuration=Release
+nuget pack ..\src\ModernWpf\ModernWpf.csproj -IncludeReferencedProjects -Prop Configuration=Release
+pause
